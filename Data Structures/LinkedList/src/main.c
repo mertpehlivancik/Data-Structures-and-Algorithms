758b2cae7073f984f659a4859798a259e4c7d13f
Mert Pehlivancık <m.pehlivancik@gmail.com>
2021-02-01 19:14:44 +0300

    getNth functions will be written.

--- Data Structures/LinkedList/src/main.c
+++ Data Structures/LinkedList/src/main.c
@@ -9,6 +9,25 @@
  */
 #include "LinkedList.h"
 
+//int linked_list_getNth_data(LINKED_LIST list, int getNth){
+//	LINKED_LIST_NODE node = list->head;
+//		for(int i=0;i<getNth;i++){
+//			node = node->next;
+//		}
+//		return node->data;
+//
+//}
+//
+//LINKED_LIST_NODE linked_list_getNth_node(LINKED_LIST list, int getNth){
+//	LINKED_LIST_NODE node = list->head;
+//	int length = linked_list_find_length_recursive(node);
+//	if(getNth)
+//	for(int i=0;i<getNth;i++){
+//		node = node->next;
+//	}
+//	return node->data;
+//}
+
 int main(void) {
 	LINKED_LIST mylist;
 	mylist = linked_list_init();
@@ -33,8 +52,9 @@ int main(void) {
 			printf("Yes\n") : printf("No\n");
 	linked_list_search_element_recursive(mylist->head, 5) ?
 			printf("Yes\n") : printf("No\n");
-	linked_list_print(mylist);
 
+//	printf("%d\n", linked_list_getNth_data(mylist, 5));
+	linked_list_print(mylist);
 	return 0;
 }
 
